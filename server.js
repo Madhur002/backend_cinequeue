4a5649654c28e44fc079f11b9cfcaa74a64934e4
Madhur002 <madhursharma2001@outlook.com>
2024-06-18 20:28:28 +0530

    update

--- server.js
+++ server.js
@@ -98,7 +98,8 @@ app.get("/movies", async (req, res) => {
 app.get("/usermovies", async (req, res) => {
   const { userId } = req.body;
   try {
-    const movies = await UserMovies.find({ userId });
+    const objectId = mongoose.Types.ObjectId(userId);
+    const movies = await UserMovies.find({ userId: objectId });
     res.json(movies);
   } catch (err) {
     res.status(500).json({ message: err.message });
